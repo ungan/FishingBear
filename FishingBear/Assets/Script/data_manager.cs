fa2698d2ffefb4f2603207118528237615cad914
ungan <83686779+ungan@users.noreply.github.com>
2024-07-28 17:28:18 +0900

    .
    q

--- FishingBear/Assets/Script/data_manager.cs
+++ FishingBear/Assets/Script/data_manager.cs
@@ -7,6 +7,9 @@ public class data_manager : MonoBehaviour
 {
     public int money;
     public int earned_at_once;      // 한번에 버는 돈
+    public int earned_at_fishing_trap = 1; // 통발로 버는 돈
+    public float fishing_trap_cooltime = 10;   // 통발에 물고기가 잡히는 시간
+    public int fishing_trap_count = 1;      // 통발에 물고기가 저장되는 회수
     public TMP_Text money_text;     // 향후 업데이트 되는 text 따로 관리 할거면 분리 할것
 
     // Start is called before the first frame update
@@ -14,6 +17,9 @@ public class data_manager : MonoBehaviour
     {
         earned_at_once = 1;
         money_text.text = "0";
+        earned_at_fishing_trap = 1;
+        fishing_trap_cooltime = 10;
+        fishing_trap_count = 1;
     }
 
     // Update is called once per frame
